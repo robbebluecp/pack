4598dddf840db8c1b7931dea26e423a14cf8d481
Yvan <yfyvan@gmail.com>
2018-03-23 15:02:49 +0800

    crawl.py update 201803231502

--- crawl.py
+++ crawl.py
@@ -1,105 +1,94 @@
 import urllib.request
+import time
+import pthread
 import random
-import urllib.parse
+import log
+import gzip
+import http.client
+import urllib.error
 
 
-def crawl(url, **kwargs):
+def crawl(url, timeout=5, encoding='utf8', maxtime=5, isProxy=False, proxyConfig=None, crawlConfig=None, urlConfig=None,
+          **kwargs):
+    urlConfig_ = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0',
+                  'Referer': '',
+                  'Host': '',
+                  'Cookie': ''}
 
-    """
+    crawlConfig_ = {'timeout': timeout,
+                    'encoding': encoding,
+                    'maxtime': maxtime}
 
-    This package includes one function for url decoding.
+    protocol = url[:url.find(':')]
 
-    :param url          :           url
-    :param kwargs       :           params for request or response or decoding
+    if not urlConfig:
+        urlConfig_.update(
+            {x.replace('_', '-'): kwargs[x] for x in kwargs if 65 <= ord(str(x)[0]) <= 90 and kwargs[x] != ''})
+    else:
+        urlConfig_.update({x: urlConfig[x] for x in urlConfig if 65 <= ord(str(x)[0]) <= 90 and urlConfig[x] != ''})
+    if not crawlConfig:
+        crawlConfig_.update({x: kwargs[x] for x in kwargs if
+                             type(kwargs[x]) == str and kwargs[x] is not None and x in ['maxtime', 'tiemout',
+                                                                                        'encoding']})
+    else:
+        crawlConfig_.update({x: kwargs[x] for x in crawlConfig if
+                             type(crawlConfig[x]) == str and crawlConfig[x] is not None and x in ['maxtime', 'tiemout',
+                                                                                                  'encoding']})
 
-    By default, we support some params for you to use, but if you want to change, just deliver what you want
-    to use with param kwargs.
+    index = 0
+    while index <= crawlConfig_['maxtime']:
 
-    For example:
-        crawl(url, timeout=5, maxtime=10, isProxy=True, proxyMode=1)
-
-        Explanation: this example means that the max wait time per request is 5 second,
-                     times for another request when request failed is 10,
-                     and we choose to use proxy to request for response with proxyMode 1.
-
-    :param timeout       :          wait time for request
-    :param maxtime       :          max time for trial to request
-    :param encoding      :          decode way for decoing html
-    :param isProxy       :          whether or not choose to use proxy
-    :param proxyMode     :          if we choose to use proxy, proxyMode must be deliver
-
-    And some other params we won't describe detaily
-
-    """
-
-    crawlConfig = {'maxtime': 5, 'timeout': 10, 'isProxy': False, 'encoding': 'utf8'}
-    crawlConfig.update(kwargs)
-
-    urlConfig = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0',
-                 'Referer': '',
-                 'Cookie': ''}
-
-    urlConfig.update({x: kwargs[x] for x in kwargs if 65 <= ord(str(x)[0]) <= 90 and kwargs[x] != ''})
-
-    headerList = list(sorted(urlConfig.items(), key=lambda x: x[0]))
-
-    urlProtocal = url[:url.find(":")]
-
-    try:
-        if crawlConfig['isProxy']:
-            try:
-                if crawlConfig['proxyMode'] == 1:
-                    proxyList = crawlConfig['urlRemarks']['proxyContent']
-                    proxyIndex = random.randrange(len(proxyList))
-                    proxyUser = proxyList[proxyIndex][0]
-                    proxyPassword = proxyList[proxyIndex][1]
-                    proxyHost = crawlConfig['proxyHost']
-                    proxyPort = crawlConfig['proxyPort']
-                    proxyData = """%(urlProtocal)s://%(proxyUser)s:%(proxyPassword)s@%(proxyHost)s:%(proxyPort)s""" % {
-                        'urlProtocal': urlProtocal,
-                        'proxyUser': proxyUser,
-                        'proxyPassword': proxyPassword,
-                        'proxyHost': proxyHost,
-                        'proxyPort': proxyPort
-                    }
-                    proxyOperation = urllib.request.ProxyHandler({'%s' % urlProtocal: '%s' % proxyData})
-                    opener = urllib.request.build_opener(proxyOperation)
-                if crawlConfig['proxyMode'] == 2:
-                    requestRawInfo = {}
-                    for info in headerList:
-                        requestRawInfo[info[0]] = info[1]
-                    requestRawInfo['url'] = url
-                    requestDecodeInfo = urllib.parse.urlencode(requestRawInfo)
-                    url = 'http://proxy.venndata.cn/crawl?' + requestDecodeInfo
-                    opener = urllib.request.build_opener()
-            except:
-                opener = urllib.request.build_opener()
-        else:
-            opener = urllib.request.build_opener()
-
-    except:
-        opener = urllib.request.build_opener()
-    opener.addheaders = headerList
-    try:
-        maxtime = crawlConfig['maxtime']
-    except:
-        maxtime = 5
-    while maxtime > 0:
         try:
-            rawHtml = opener.open(url, timeout=crawlConfig['timeout'])
-            if rawHtml.code != 200:
-                maxtime -= 1
-                html = None
+            if isProxy:
+                # 自动从代理池获取
+                if not proxyConfig:
+                    proxy = random.choice(pthread.proxyList)
+                    proxyData = 'http://' + proxy
+                else:
+                    # proxyConfig = {'type': 1, 'proxyInfo': [your ips,type list]}
+                    if str(proxyConfig['type']) == '1':  # 自定义代理池
+                        proxyList = proxyInfo = proxyConfig['proxyInfo']
+                        proxy = random.choice(proxyList)
+                        proxyData = 'http://' + proxy
+                    # proxyConfig = {'type': 2, 'proxyInfo':[{'host':'http-dyn.abuyun.com','port':'9020', 'user':'', 'password':''},{'host':'http-dyn.abuyun.com','port':'9020', 'user':'', 'password':''}]}
+                    if str(proxyConfig['type']) == '2':  # abuyun代理
+                        proxyInfo = proxyConfig['proxyInfo']
+                        proxy = random.choice(proxyInfo)
+                        user = proxyInfo['user']
+                        password = proxyInfo['password']
+                        host = proxyConfig['host']
+                        port = proxyInfo['port']
+                        proxyData = '%(user)s:%(password)s@%(host)s:%(port)s' % {'user': user, 'password': password,
+                                                                                 'host': host, 'port': port}
+                proxyHandler = urllib.request.ProxyHandler({protocol: proxyData})
+                opener = urllib.request.build_opener(proxyHandler)
             else:
-                try:
-                    html = rawHtml.read().decode(crawlConfig['encoding'], errors='ignore')
-                    return html
-                except Exception as e:
-                    maxtime -= 1
-                    html = None
-        except Exception as e:
-            print(e)
-            maxtime -= 1
-            html = None
+                opener = urllib.request.build_opener()
 
-    return html
+
+            try:
+                rawHtml = opener.open(url, timeout=crawlConfig_['timeout'])
+                if rawHtml.code != 200:
+                    html = None
+                    assert rawHtml.code == 200,'返回码%s不等于200,url:%s'% (rawHtml.code,url)
+                else:
+                    bytes = rawHtml.read()
+                    try:
+                        data = gzip.decompress(bytes)
+                    except:
+                        data = bytes
+                    html = data.decode(crawlConfig_['encoding'], errors='ignore')
+                    return html
+            except http.client.BadStatusLine:
+                index += 1
+                log.critical('BadStatusLine Error, URL:' % url)
+            except urllib.error.URLError as e:
+                index += 0.2
+                log.error('URLError, URL:%s, ERROR:%s' % (url, e))
+            except Exception as e:
+                log.error('Other Error, URL:%s, ERROR:%s'% (url, e))
+            log.error('Index is over than %s times,crawl fail, URL;%s' % (crawlConfig_['maxtime'], url))
+            return None
+        except Exception as e:
+            log.critical('...', e)
+            return None
