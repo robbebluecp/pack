fe3d2e674472550b9f3e81e5d615e5485aeef439
Yvan <yfyvan@gmail.com>
2018-06-06 15:42:50 +0800

    bug modify
    __file__   -> sys.argv[0]

--- log.py
+++ log.py
@@ -1,6 +1,7 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import os
+import sys
 
 """
 NOTSET      0
@@ -11,7 +12,8 @@ ERROR       40
 CRITICAL    50
 """
 
-path = os.getcwd() + '/'
+path = (os.path.dirname(sys.argv[0]) + '/').replace('//', r'/')
+print(path)
 
 warningFilter = logging.Filter()
 errorFilter = logging.Filter()
@@ -20,17 +22,17 @@ warningFilter.filter = lambda level: 30 <= level.levelno <= 30
 errorFilter.filter = lambda level: 40 <= level.levelno <= 40
 criticalFilter.filter = lambda level: 50 <= level.levelno <= 50
 
-warningOperation = RotatingFileHandler(path + 'Z_ExWarning.log', maxBytes=5 * 1024 * 1024, backupCount=1)
+warningOperation = RotatingFileHandler(path + 'Z_ExWarning.log', maxBytes=1 * 1024 * 1024, backupCount=1)
 warningOperation.addFilter(warningFilter)
 warningFormat = logging.Formatter('%(asctime)s %(filename)s %(levelname)s %(message)s')
 warningOperation.setFormatter(warningFormat)
 
-errorOperation = RotatingFileHandler(path + 'Z_ExError.log', maxBytes=5 * 1024 * 1024, backupCount=5)
+errorOperation = RotatingFileHandler(path + 'Z_ExError.log', maxBytes=1 * 1024 * 1024, backupCount=5)
 errorOperation.addFilter(errorFilter)
 errorFormat = logging.Formatter('%(asctime)s %(filename)s %(levelname)s %(message)s')
 errorOperation.setFormatter(errorFormat)
 
-criticalOperation = RotatingFileHandler(path + 'Z_ExCritical.log', maxBytes=5 * 1024 * 1024, backupCount=2)
+criticalOperation = RotatingFileHandler(path + 'Z_ExCritical.log', maxBytes=1 * 1024 * 1024, backupCount=2)
 criticalOperation.addFilter(criticalFilter)
 criticalFormat = logging.Formatter('%(asctime)s %(filename)s %(levelname)s %(message)s')
 criticalOperation.setFormatter(criticalFormat)
@@ -48,9 +50,9 @@ log.addHandler(errorOperation)
 log.addHandler(criticalOperation)
 log.addHandler(console)
 
-
 debug = log.debug
 info = log.info
 warning = log.warning
 error = log.error
 critical = log.critical
+
