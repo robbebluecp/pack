9e4e0115d24d9739e44d8c20450b8f38b3dcb28b
Yvan <yfyvan@gmail.com>
2018-09-25 12:24:21 +0800

    add softmax  sigmoid and time_stamp

--- extend.py
+++ extend.py
@@ -1,5 +1,5 @@
 import ctypes
-
+import time
 
 def kill(tid, theType=SystemExit):
     try:
@@ -10,3 +10,15 @@ def kill(tid, theType=SystemExit):
     except Exception as e:
         print('Fail to kill thread, Error is : ' + str(e))
 
+def softmax(x_input):
+    x_input = np.asarray(x_input, dtype=np.float) / max(x_input)
+    return np.exp(x_input) / np.sum(np.exp(x_input))
+
+
+def sigmoid(x_input):
+    return 1.0 / (1.0 + np.exp(-(np.asarray(x_input, dtype=np.float))))
+
+def time_stamp(time_int):
+    chTime = time.localtime(time_int)
+    output = time.strftime("%Y--%m--%d %H:%M:%S", chTime)
+    return output
