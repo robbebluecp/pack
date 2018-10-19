2d0dcb74639bb73d5b7d921be5db54f6be61dddd
Yvan <yfyvan@gmail.com>
2018-10-19 18:35:43 +0800

    add parseconfig(2018/10/19 18:30)
    parseconfig is a diy class tha t inherits from configparse.ConfigParse, but more convenient for python to parse config files into dicts.
    bool type params in config files(ini,cfg and ...) still can't be transform into  python bools type , so i advice you to use 0 or 1 instead.

---
+++ parseconfig.py
@@ -0,0 +1,63 @@
+import configparser
+import os
+
+
+class ParseConfig(configparser.ConfigParser, configparser.NoSectionError):
+    def __init__(self):
+        super(ParseConfig, self).__init__()
+
+    def optionxform(self, optionstr):
+        return optionstr
+
+    def read(self, filenames, encoding='utf8'):
+        if isinstance(filenames, (str, os.PathLike)):
+            filenames = [filenames]
+        read_ok = []
+        for filename in filenames:
+            try:
+                with open(filename, encoding=encoding) as fp:
+                    self._read(fp, filename)
+            except OSError:
+                continue
+            if isinstance(filename, os.PathLike):
+                filename = os.fspath(filename)
+            read_ok.append(filename)
+        return read_ok
+
+    def items(self, section=object(), raw=False, vars=None):
+        if section is object():
+            return super().items()
+        d = self._defaults.copy()
+        try:
+            d.update(self._sections[section])
+            d = dict(d)
+            for i in d:
+                try:
+                    value = int(d[i])
+                except:
+                    value = d[i]
+                d[i] = value
+            return d
+        except KeyError:
+            if section != self.default_section:
+                raise configparser.NoSectionError(section)
+
+
+def parse(section, fileName='config.cfg'):
+    parser = ParseConfig()
+    parser.read(fileName)
+    return parser.items(section)
+
+
+class Parse(dict):
+    crawlConfig = parse('crawlConfig')
+    proxyConfig = parse('proxyConfig')
+    dstConfig = parse('dstCon')
+    generateConfig = parse('generateConfig')
+    baseInfo = parse('baseInfo')
+    spyderConfig = parse('spyderConfig')
+    urlConfig = parse('urlConfig')
+    # dstCon = parse('dstCon')
+    srcTable = parse('srcTable')
+    srcCon = parse('srcCon')
+
