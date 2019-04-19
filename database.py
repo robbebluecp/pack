957b6355eac2c76dd26ef106bd1d2b0153cfa649
ldhsight <luozy@pbcsf.tsinghua.edu.cn>
2019-04-19 20:30:21 +0800

    bug fix

--- database.py
+++ database.py
@@ -29,8 +29,9 @@ class Database:
 
     def __init__(self, dbConfig={'host': 'localhost', 'user': 'root', 'password': '321', 'dbname': 'tmp', 'mode': 1, 'charset': 'utf8'}, **kwargs):
         self.dbConfig = dbConfig
+        self.dbConfig['charset'] = 'utf8'
         if kwargs:
-            self.dbConfig.update({x: kwargs.get(x) for x in ['host', 'user', 'password', 'mode', 'dbname','tbname', 'charset'] if kwargs.get(x) != '' and kwargs.get(x) is not None})
+            self.dbConfig.update({x: kwargs.get(x) if kwargs.get(x) != '' and kwargs.get(x) is not None else dbConfig[x] for x in ['host', 'user', 'password', 'mode', 'dbname','tbname', 'charset'] })
         self.con = self.getCon(self.dbConfig)
         self.cur = self.con.cursor()
 
